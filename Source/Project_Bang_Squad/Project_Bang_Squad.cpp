31897e56aa80aa695921662f3042d94d78e279ad
ChoiShinWon <csw57421@gmail.com>
2026-01-05 12:57:50 +0900

    C++ 코드 승격

---
+++ Source/Project_Bang_Squad/Project_Bang_Squad.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Project_Bang_Squad.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_Bang_Squad, "Project_Bang_Squad" );
