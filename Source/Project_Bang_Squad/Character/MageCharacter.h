2a452db71dc03c69047102d80ecdc39c22e89a9c
ChoiShinWon <csw57421@gmail.com>
2026-01-08 17:52:16 +0900

    Revert "Reapply "메이지 염력 능력""
    This reverts commit 9138227903917b8745adcf709cb5a500ec001612.

--- Source/Project_Bang_Squad/Character/MageCharacter.h
+++ Source/Project_Bang_Squad/Character/MageCharacter.h
@@ -4,8 +4,9 @@
 #include "Project_Bang_Squad/Character/Base/BaseCharacter.h" 
 #include "MageCharacter.generated.h"
 
+// 클래스 전방 선언
 class UDataTable;
-class APillar; // Pillar 클래스 전방 선언
+class UPrimitiveComponent;
 
 UCLASS()
 class PROJECT_BANG_SQUAD_API AMageCharacter : public ABaseCharacter
@@ -15,19 +16,21 @@ class PROJECT_BANG_SQUAD_API AMageCharacter : public ABaseCharacter
 public:
 	AMageCharacter();
 
+
 	virtual void Tick(float DeltaTime) override;
+	
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 protected:
-	virtual void BeginPlay() override;
-	
 	/** 스킬 기능 재정의 */
 	virtual void Attack() override;
 	virtual void Skill1() override;
 	virtual void Skill2() override;
     
-	// JobAbility(우클릭): 타겟 고정 및 조작 시작
+	// JobAbility(우클릭): 단발성 염력 타격(Impulse)
 	virtual void JobAbility() override;
+
+	// EndJobAbility: 우클릭 뗌 (힘 적용 중단)
 	void EndJobAbility();
 
 	/** 데이터 테이블 */
@@ -35,33 +38,28 @@ protected:
 	UDataTable* SkillDataTable;
 
 private:
-	// 에디터에서 설정한 기본 팔 길이와 오프셋을 저장할 변수
-	float DefaultArmLength;
-	FVector DefaultSocketOffset;
-	
 	/** 내부 로직 함수 */
-	void ProcessSkill(FName SkillRowName);
     
-	// 매 프레임 실행: 크로스헤어 아래 Pillar 확인 및 아웃라인 처리
-	void UpdatePillarInteraction();
-
-	// 시선 고정 로직
-	void LockOnPillar(float DeltaTime);
+	// 데이터 테이블 기반 스킬 실행
+	void ProcessSkill(FName SkillRowName);
 	
-	// 카메라 복귀 지연을 위한 타이머 핸들
-	FTimerHandle CameraResetTimerHandle;
+	// 매 프레임 실행: 크로스헤어 아래 물체 확인 및 아웃라인 처리
+	void UpdateCrosshairInteraction();
 
-	// 염력 사거리
+	// 염력 사거리 (기존 700 -> 1500으로 증가, 멀리서도 쓰러뜨리기 위해)
 	UPROPERTY(EditAnywhere, Category = "Telekinesis")
-	float TraceDistance = 5000.f;
+	float TraceDistance = 1500.f;
 
-	// 현재 메이지가 조준/조작 중인 기둥 액터
+	// 염력 밀기 강도 (새로 추가됨)
+	UPROPERTY(EditAnywhere, Category = "Telekinesis")
+	float PushForce = 5000000.f;
+	
+	// 현재 내가 우클릭으로 잡고(밀고)있는 물리 컴포넌트
 	UPROPERTY()
-	APillar* FocusedPillar;
-
+	UPrimitiveComponent* CurrentTargetComp;
+	
+	// 현재 시선이 닿아 아웃라인(테두리)이 켜진 컴포넌트
 	UPROPERTY()
-	APillar* CurrentTargetPillar;
-
-	// 조작 중인지 여부
-	bool bIsJobAbilityActive = false;
+	UPrimitiveComponent* CurrentFocusedComp;
+	
 };
