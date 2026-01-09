d20630c0f14382f5f67e3d0a876d27dad790ce5f
ChoiShinWon <csw57421@gmail.com>
2026-01-09 13:51:26 +0900

    메이지 직업능력 구현완료 및 서버 동기화 성공!
    프로젝타일이랑 기둥 떨어지는것도 연동 됨

--- Source/Project_Bang_Squad/Character/Pillar.h
+++ Source/Project_Bang_Squad/Character/Pillar.h
@@ -8,40 +8,43 @@ UCLASS()
 class PROJECT_BANG_SQUAD_API APillar : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+    
+public: 
 	APillar();
+
+	// 동기화할 변수 등록 함수 (필수)
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
 protected:
 	virtual void BeginPlay() override;
 
-public:	
-	// 기둥의 메쉬
+public: 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	class UStaticMeshComponent* PillarMesh;
 
-	// 메이지가 이 기둥을 봤을 때 띄울 안내 문구
 	UPROPERTY(EditAnywhere, Category = "MageAbility")
 	FString InteractionText = TEXT("우클릭 + 마우스 왼쪽으로!");
 
-	// 넘어뜨리기 위해 필요한 마우스 방향 (-1: 왼쪽, 1: 오른쪽)
 	UPROPERTY(EditAnywhere, Category = "MageAbility")
 	float RequiredMouseDirection = -1.0f;
 
-	// 넘어지는 방향 (에디터에서 화살표로 조절 가능)
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MageAbility", meta = (MakeEditWidget = true))
 	FVector FallDirection = FVector(500.f, 0.f, 0.f);
 
-	// 넘어지는 힘의 세기
 	UPROPERTY(EditAnywhere, Category = "MageAbility")
 	float FallForce = 1000000.f;
 
+	// [중요] ReplicatedUsing: 이 변수가 서버에서 바뀌면 클라이언트에서 OnRep_IsFallen 함수가 자동 실행됨
+	UPROPERTY(ReplicatedUsing = OnRep_IsFallen, VisibleAnywhere, BlueprintReadOnly, Category = "State")
 	bool bIsFallen = false;
 
-	// 메이지가 기둥을 넘어뜨리는 함수
+	// 상태 변경 감지 함수 (클라이언트에서 실행됨)
+	UFUNCTION()
+	void OnRep_IsFallen();
+
+	// 메이지가 호출할 함수 (서버에서만 실행되어야 함)
 	void TriggerFall();
 
-	// 바닥에 닿았을 때 실행될 함수
 	UFUNCTION()
 	void OnPillarHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 };
--- Source/Project_Bang_Squad/Projectile/MageProjectile.h
+++ Source/Project_Bang_Squad/Projectile/MageProjectile.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -10,36 +8,33 @@ UCLASS()
 class PROJECT_BANG_SQUAD_API AMageProjectile : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
-	
+    
+public: 
 	AMageProjectile();
 
 protected:
 	virtual void BeginPlay() override;
-	
+    
 	/** 투사체의 충돌 범위 (Root) */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	class USphereComponent* SphereComp;
-	
+    
 	/** 투사체의 외형 (Mesh) */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	class UStaticMeshComponent* MeshComp;
-	
+    
 	/** 투사체의 이동을 제어하는 컴포넌트 */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	class UProjectileMovementComponent* ProjectileMovement;
-	
-	/** 무언가에 부딪혔을 때 호출되는 함수 */
-	// Overlap 이벤트용 함수 시그니처
+    
+	/** 무언가에 부딪혔을 때 호출되는 함수 (서버/클라이언트 모두 호출될 수 있음) */
 	UFUNCTION()
 	void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, 
-		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, 
-		bool bFromSweep, const FHitResult& SweepResult);
-	
-public:	
+	   UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, 
+	   bool bFromSweep, const FHitResult& SweepResult);
+    
+public: 
 	/** 캐릭터(Mage)가 생성할 때 전달해 줄 데미지 값*/
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Damage")
-	float Damage;
-
+	float Damage = 20.0f; // 기본값 설정
 };
