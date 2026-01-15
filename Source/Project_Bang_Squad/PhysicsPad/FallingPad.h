a51079664345943aa34cb7e9aee13fa91bb2a06d
Jangsaehyeon <s1206h@naver.com>
2026-01-15 15:33:02 +0900

    Add FallingPad and RotatingPad actors for PhysicsPad
    Introduced new FallingPad and RotatingPad C++ classes with corresponding headers and assets for interactive physics-based pads. Updated related level and blueprint assets to integrate these new actors, enabling falling and rotating platform mechanics in the game.

---
+++ Source/Project_Bang_Squad/PhysicsPad/FallingPad.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FallingPad.generated.h"
+
+UCLASS()
+class PROJECT_BANG_SQUAD_API AFallingPad : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    AFallingPad();
+
+protected:
+    virtual void BeginPlay() override;
+
+    UFUNCTION()
+    void OnBoxOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+public:
+    UPROPERTY(VisibleAnywhere, Category = "Components")
+    class UStaticMeshComponent* MeshComp;
+
+    UPROPERTY(VisibleAnywhere, Category = "Components")
+    class UBoxComponent* DetectionBox;
+
+    UPROPERTY(EditAnywhere, Category = "Settings")
+    float FallDelay = 0.1f;
+
+private:
+    FTimerHandle FallTimerHandle;
+
+    void StartFalling();
+};
